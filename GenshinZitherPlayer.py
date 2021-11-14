75e78775ed1d4b506d45c8ea97ee2e06a08be9e7
LinYuanChan <msz13399173387@gmail.com>
2021-11-14 11:53:31 +0800

    Add C trans

--- GenshinZitherPlayer.py
+++ GenshinZitherPlayer.py
@@ -7,25 +7,44 @@ import pyautogui
 # Key Map between midi file and Genshin Keyboard
 KEY_MAP = {60:'z',62:'x',64:'c',65:'v',67:'b',69:'n',71:'m',72:'a',74:'s',76:'d',77:'f',79:'g',81:'h',83:'j',84:'q',86:'w',88:'e',89:'r',91:'t',93:'y',95:'u',0:'p'}
 
+SCALES = ["C","C#","D","D#","E","F","F#","G","G#","A","A#","B"]
+
 # Turn off autopause in pyautogui
 pyautogui.PAUSE = 0
 
 # Translate midi note to keybord
 def noteTrans(m_note_value):
-    return KEY_MAP[int(m_note_value)]
+    return KEY_MAP[m_note_value]
 
 # Genshin's zither only support melody in C Major, Use this to translate other scale to C Major
-# def allToCMajor(m_file_name):
-#     note_temp = []
-#     for msg in mido.MidiFile(m_file_name+".mid"):
-#         if(msg.type == "note_on" or msg.type == "note_off"):
-#             if(note_temp.type): 
-#                 # todo
-#                 note_temp.append(int(msg.note))
+def allToCMajor(m_file_name):
+    note_temp = []
+    avialiable_add = []
+    for msg in mido.MidiFile(m_file_name):
+        if(msg.type == "note_on" or msg.type == "note_off"):
+            isExist = False
+            for i in note_temp:
+                if(i==msg.note):
+                    isExist = True
+                    break;
+            if(not isExist): 
+                note_temp.append(msg.note)
+    
+
+    for i in range(-12,13):
+        isC = True
+        for cur in note_temp:
+            if(KEY_MAP.__contains__(int(cur)+i)==False):
+                isC = False
+                break
+        if(isC):
+            avialiable_add.append(i)
+    return avialiable_add
+            
         
         
 
-def playMidi(m_file_name, m_bpm):
+def playMidi(m_file_name, m_bpm, m_key_add):
     # Trans bpm to spb ( Second per bar )
     spb = float(60 / m_bpm * 2) 
     
@@ -33,14 +52,12 @@ def playMidi(m_file_name, m_bpm):
     for msg in mido.MidiFile(m_file_name):
         if(msg.type=="note_on"):
             # Press
-            print(msg.type, msg.note, msg.time)
             pyautogui.sleep(float(msg.time)*spb)
-            pyautogui.keyDown(noteTrans(msg.note))
+            pyautogui.keyDown(noteTrans(int(msg.note)+m_key_add))
         elif(msg.type=="note_off"):
             # Release
-            print(msg.type, msg.note, msg.time, "\n")
             pyautogui.sleep(float(msg.time)*spb)
-            pyautogui.keyUp(noteTrans(msg.note))
+            pyautogui.keyUp(noteTrans(int(msg.note)+m_key_add))
         else:
             continue
 
@@ -51,7 +68,40 @@ def counter(m_second):
         time.sleep(1)        
                
 if __name__ == '__main__':
-    file_name = input("Select midi file: ")
+    # init
+    note_add = 0
+    isC = False
+    
+    # Input midi file path and bpm
+    file_name = "." + os.sep+ "midi_repo" + os.sep + input("Select midi file: ") + ".mid"
     bpm = int(input("Input bpm: "))
+    
+    # Detect scale
+    key_add = allToCMajor(file_name)
+    if(not(key_add)):
+        exit("The midi file is too complex to play!")
+        
+    if(len(key_add) == 1):
+        if(SCALES[key_add[0]] == "C"):
+            isC = True
+    
+    
+    # Select avialable scale
+    if(not isC):
+        print("Choose key add values:")
+        op_num = 1
+        for i in key_add:
+            print("%d.Play %+dkeys" %(op_num,i))
+            op_num += 1
+
+        selected_op_num = int(input(">"))
+        op_num = 1
+        for i in key_add:
+            if(op_num == selected_op_num):
+                note_add = i
+            op_num += 1
+    
     counter(5)
-    playMidi(file_name,bpm)
+    print("Playing \"%s\"..." % file_name)
+    playMidi(file_name,bpm,note_add)
+    print("Done.")
